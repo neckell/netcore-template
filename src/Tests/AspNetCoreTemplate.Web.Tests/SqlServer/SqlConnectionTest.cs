42e5ce387804a9beed56cc6808a05cf5d771c8f1
Nicolas Eckell <niceckell@gmail.com>
2023-06-08 16:08:26 -0300

    Initial commit

---
+++ src/Tests/AspNetCoreTemplate.Web.Tests/SqlServer/SqlConnectionTest.cs
@@ -0,0 +1,50 @@
+﻿namespace AspNetCoreTemplate.Web.Tests.SqlServer
+{
+    using System;
+    using System.IO;
+    using Microsoft.Data.SqlClient;
+    using Microsoft.Extensions.Configuration;
+    using Xunit;
+
+    public class SqlConnectionTest
+    {
+        private readonly IConfiguration _configuration;
+
+        public SqlConnectionTest()
+        {
+            // Load the configuration from appsettings.json
+            this._configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+        }
+
+        [Fact]
+        public void Test_Connection_Ok()
+        {
+            // Arrange
+            string connectionString = _configuration.GetConnectionString("DefaultConnection");
+
+            // Act
+            bool isConnected = this.TestConnection(connectionString);
+
+            // Assert
+            Assert.True(isConnected, "Connection failed.");
+        }
+
+        private bool TestConnection(string connectionString)
+        {
+            try
+            {
+                using SqlConnection connection = new(connectionString);
+                connection.Open();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error occurred while testing connection: " + ex.Message);
+                return false;
+            }
+        }
+    }
+
+}
